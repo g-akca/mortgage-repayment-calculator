f4749a49821f639a7ba612e208278429f4b2eea4
g-akca <gok.akca66@gmail.com>
2026-03-13 08:43:13 +0300

    Add empty ResultsSection

--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -1,7 +1,12 @@
+import emptyImg from "/assets/images/illustration-empty.svg";
+
 function ResultsSection() {
   return (
-    <>
-    </>
+    <section className="py-8 px-6 bg-slate-900 gap-4 flex flex-col items-center text-center">
+      <img src={emptyImg} className="w-48 h-48" />
+      <h2 className="text-2xl leading-[125%] font-bold text-white">Results shown here</h2>
+      <p className="text-slate-300">Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
+    </section>
   )
 }
 export default ResultsSection;
