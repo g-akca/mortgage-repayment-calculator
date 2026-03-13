29c7d40562cf8a9aa02bd09e1c7b84f158b35de5
g-akca <gok.akca66@gmail.com>
2026-03-14 02:38:45 +0300

    Add tablet responsive styles to App

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,13 @@ function App() {
   const [results, setResults] = useState(null);
 
   return (
-    <div className="min-h-screen bg-slate-100 text-base font-medium leading-base text-slate-700">
-      <main>
+    <div 
+      className="
+        min-h-screen bg-slate-100 text-base font-medium leading-base text-slate-700 
+        tablet:p-10 tablet:flex tablet:items-center tablet:justify-center
+      "
+    >
+      <main className="min-h-screen overflow-hidden flex flex-col tablet:rounded-3xl tablet:min-h-0">
         <MortgageForm setResults={setResults} />
         <ResultsSection results={results} />
       </main>
--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -3,7 +3,7 @@ import emptyImg from "/assets/images/illustration-empty.svg";
 function ResultsSection({ results }) {
   return (
     results ? (
-      <section className="py-8 px-6 bg-slate-900 gap-6 flex flex-col">
+      <section className="py-8 px-6 bg-slate-900 gap-6 flex flex-col grow">
         <div>
           <h2 className="mb-4 text-white text-2xl font-bold leading-[125%]">Your results</h2>
           <p className="text-slate-300">Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
@@ -23,7 +23,7 @@ function ResultsSection({ results }) {
       </section>
     )
     : (
-      <section className="py-8 px-6 bg-slate-900 gap-4 flex flex-col items-center text-center">
+      <section className="py-8 px-6 bg-slate-900 gap-4 flex flex-col items-center text-center grow">
         <img src={emptyImg} className="w-48 h-48" />
         <h2 className="text-2xl leading-[125%] font-bold text-white">Results shown here</h2>
         <p className="text-slate-300">Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
